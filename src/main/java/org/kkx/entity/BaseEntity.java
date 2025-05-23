efca7b59a3df45346f1046069b568c28341e369e
ly1994lyy <1141730852@qq.com>
2025-05-23 21:04:42 +0800

    fix: 未返回创建时间和更新时间

--- src/main/java/org/kkx/entity/BaseEntity.java
+++ src/main/java/org/kkx/entity/BaseEntity.java
@@ -3,11 +3,15 @@ package org.kkx.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 abstract class BaseEntity {
